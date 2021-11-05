bf61d823d2bf6e142dd75dc2b69389e50abff99b
Nayeon Kim <toni@Nayeons-MacBook-Air.local>
2021-11-05 14:01:08 +0900

    improved ui and added corresponding store icons

--- cookie-clicker/index.js
+++ cookie-clicker/index.js
@@ -19,6 +19,22 @@ const updateTitle = () => {
     }, 1000)
 }
 
+// image elements
+const imgCur = document.querySelector('.img.hidden.cursor');
+const imgPer = document.querySelector('.img.hidden.granny');
+const imgMkr = document.querySelector('.img.hidden.maker');
+const imgBkr = document.querySelector('.img.hidden.bakery');
+const imgCrp = document.querySelector('.img.hidden.corp');
+const imgEmp = document.querySelector('.img.hidden.empress');
+
+// image p elements
+const showCur = document.querySelector('.showCur')
+const showGran = document.querySelector('.showGran')
+const showMkr = document.querySelector('.showMkr')
+const showBkr = document.querySelector('.showBkr')
+const showCrp = document.querySelector('.showCrp')
+const showEmp = document.querySelector('.showEmp')
+
 // Store elements
 const cursor = document.querySelector('#cursor')
 const person = document.querySelector('#granny');
@@ -99,10 +115,13 @@ const addCursor = (val) => {
         newCursor.purchase()
         // increments the number of cursors purchased
         numCursor.innerText = parseInt(numCursor.innerText) + 1
+        // sync num cursors
+        showCur.innerText = numCursor.innerText
         // increases the cost
         curCost.innerText = val
         // reveals the next level element
         person.classList.remove('hidden')
+        imgCur.classList.remove('hidden')
     }
 }
 
@@ -120,8 +139,10 @@ const addPerson = (val) => {
     if (numBread.innerText >= newPerson.cost) {
         newPerson.purchase()
         numPerson.innerHTML = parseInt(numPerson.innerHTML) + 1
+        showGran.innerHTML = numPerson.innerHTML
         granCost.innerText = increaseCost(val)
         breadMaker.classList.remove('hidden')
+        imgPer.classList.remove('hidden')
     }
 }
 
@@ -137,8 +158,10 @@ const addBreadMaker = (val) => {
     if (numBread.innerText >= newBreadMaker.cost) {
         newBreadMaker.purchase()
         numMaker.innerHTML = parseInt(numMaker.innerHTML) + 1
+        showMkr.innerText = numMaker.innerText
         makerCost.innerText = increaseCost(val)
         bakery.classList.remove('hidden')
+        imgMkr.classList.remove('hidden')
     }
 }
 
@@ -154,15 +177,17 @@ const addBakery = (val) => {
     if (numBread.innerText >= newBakery.cost) {
         newBakery.purchase()
         numBakery.innerHTML = parseInt(numBakery.innerHTML) + 1
+        showBkr.innerText = numBakery.innerText
         bakeryCost.innerText = increaseCost(val)
         corporateChain.classList.remove('hidden')
+        imgBkr.classList.remove('hidden')
     }
 }
 
 bakery.addEventListener('click', () => {
     if (numBread.innerText >= bakeryVal) {
-        makerVal = increaseCost(bakeryVal)
-        addBreadMaker(bakeryVal)
+        bakeryVal = increaseCost(bakeryVal)
+        addBakery(bakeryVal)
     }
 })
 
@@ -171,8 +196,10 @@ const addCorporate = (val) => {
     if (numBread.innerText >= newCorporate.cost) {
         newCorporate.purchase()
         numCorp.innerHTML = parseInt(numCorp.innerHTML) + 1
+        showCrp.innerText = numCorp.innerHTML
         corpCost.innerText = increaseCost(val)
         empress.classList.remove('hidden')
+        imgCrp.classList.remove('hidden')
     }
 }
 
@@ -189,6 +216,7 @@ const addEmpress = (val) => {
         newEmpress.purchase()
         emCost.innerText = increaseCost(val)
         numEm.innerHTML = parseInt(numEm.innerHTML) + 1
+        showEmp.innerHTML = numEm.innerHTML
     }
 }
 
@@ -196,6 +224,7 @@ empress.addEventListener('click', () => {
     if (numBread.innerText >= emVal) {
         emVal = increaseCost(emVal)
         addEmpress(emVal)
+        imgEmp.classList.remove('hidden')
     }
 })
 
